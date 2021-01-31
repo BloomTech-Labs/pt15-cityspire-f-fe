32a733d25bf0bf1a9a060fc2441620713eeaf99c
Nicole Osborn <nicole.y.osborn@gmail.com>
2021-01-31 13:41:15 -0700

    linked favorites to "profile list" and added logout to the top nav

--- src/components/pages/Home/RenderHomePage.js
+++ src/components/pages/Home/RenderHomePage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from '../../common';
 import 'antd/dist/antd.css';
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Layout, Menu } from 'antd';
 
 const { Header, Content, Footer } = Layout;
 
@@ -13,33 +13,22 @@ function RenderHomePage(props) {
       <Header>
         <div className="logo" />
         <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-          <Menu.Item key="1">nav 1</Menu.Item>
-          <Menu.Item key="2">nav 2</Menu.Item>
-          <Menu.Item key="3">nav 3</Menu.Item>
+          <Menu.Item key="1">
+            <Link to="/profile-list">Favorites</Link>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Link to="/example-list">Example List of Items</Link>
+          </Menu.Item>
+          <Menu.Item key="3" onClick={() => authService.logout()}>
+            Logout
+          </Menu.Item>
         </Menu>
       </Header>
       <Content style={{ padding: '0 50px' }}>
         <div className="site-layout-content">
           <div>
-            <p>
-              This is an example of a common example of how we'd like for you to
-              approach components.
-            </p>
-            <p>
-              <Link to="/profile-list">Profiles Example</Link>
-            </p>
-            <p>
-              <Link to="/example-list">Example List of Items</Link>
-            </p>
-            <p>
-              <Link to="/datavis">Data Visualizations Example</Link>
-            </p>
-            <p>
-              <Button
-                handleClick={() => authService.logout()}
-                buttonText="Logout"
-              />
-            </p>
+            <p>This is where we will put Search component</p>
+            <p>This is where we will put map component</p>
           </div>
         </div>
       </Content>
