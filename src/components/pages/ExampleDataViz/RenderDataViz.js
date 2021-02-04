482bfda814bcaaeda21ccec1eefb896b55943d59
sablemadison <50024717+sablemadison@users.noreply.github.com>
2021-02-03 22:24:33 -0500

    Update RenderDataViz.js
    remove unused react map component

--- src/components/pages/ExampleDataViz/RenderDataViz.js
+++ src/components/pages/ExampleDataViz/RenderDataViz.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import Plot from 'react-plotly.js';
 import { getDSData } from '../../../api';
-import ReactMapGL from 'react-map-gl';
+
 
 const initialState = {
   data: [],
@@ -17,13 +17,7 @@ const initialState = {
 function DataViz(props) {
   const [data, setData] = useState(initialState);
   const [figure, setFigure] = useState(null);
-  const [viewport, setViewport] = useState({
-    latitude: 45.4211,
-    longitude: -75.6903,
-    width: '65vw',
-    height: '65vw',
-    zoom: 4,
-  });
+  
   useEffect(() => {
     function fetchDSData() {
       getDSData(props.url, props.authState)
@@ -38,16 +32,15 @@ function DataViz(props) {
   }, [props.url, props.authState]);
 
   return (
-    <ReactMapGL
-      {...viewport}
-      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-      onViewportChange={viewport => {
-        setViewport(viewport);
-      }}
-      mapStyle="mapbox://styles/cssc1/ckkg67a3000eo17s4xx1ocnvr"
-    >
-      *markers here*
-    </ReactMapGL>
+    <Plot
+       className="DataViz"
+       data={data.data}
+       layout={data.layout}
+       frames={data.frames}
+       config={data.config}
+       onInitialized={figure => setFigure(figure)}
+       onUpdate={figure => setFigure(figure)}
+     />
   );
 }
 
