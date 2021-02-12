563076bb319c542152365a07a104ac6b743c0974
Nicole Osborn <nicole.y.osborn@gmail.com>
2021-02-11 21:13:09 -0700

    added heart icon from ant design icons

--- src/components/pages/ExampleDataViz/CityInfo.js
+++ src/components/pages/ExampleDataViz/CityInfo.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import 'antd/dist/antd.css';
-import './index.css';
+// import './index.css';
 import { Card } from 'antd';
+import { HeartOutlined } from '@ant-design/icons';
 
 function CityInfo(props) {
   return (
     <div className="site-card-border-less-wrapper">
-      <Card title="City, State" bordered={false} style={{ width: 300 }}>
+      <Card
+        title={props.city + ', ' + props.state}
+        bordered={false}
+        style={{ width: 300 }}
+      >
         <p>Average Rent Price:</p>
         <p>Overall Crime Rate:</p>
         <p>Walkability Score:</p>
+        <HeartOutlined />
       </Card>
     </div>
   );
